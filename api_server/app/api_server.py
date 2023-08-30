7707e35813b054913da0eabc30f770e79738a5c0
bjoernengelmann <bjoern.engelmann@tu-dortmund.de>
2023-08-30 15:13:49 +0200

    update

--- api_server/app/api_server.py
+++ api_server/app/api_server.py
@@ -11,31 +11,49 @@ import glob
 
 app = Flask(__name__)
 Base = declarative_base() 
-session = None
+session_ds = None
+session_d2q = None
 bm25_models = {}
 indices = {}
 last_query = None
-
-class TableQuery(Base):
-    __tablename__ = 'tablequery'
-    docno = Column(String, primary_key=True)
-    querygen = Column(Text)
+    
+class WapoEntry(Base):
+    __tablename__ = 'wapo'
+    doc_id = Column(String, primary_key=True)
+    url = Column(String)
+    title = Column(String)
+    author = Column(String)
+    kicker = Column(Text)
+    body = Column(Text)
     
     def __repr__(self):
-        repr_str = f"docno={self.docno}, query={self.querygen}"
+        repr_str = f"doc_id={self.doc_id}, url={self.url}, title={self.title}, author={self.author},"\
+        f"kicker={self.kicker}, body={self.body}"
+        
+        return repr_str
+    
+class NytEntry(Base):
+    __tablename__ = 'nyt'
+    doc_id = Column(String, primary_key=True)
+    headline = Column(String)
+    body = Column(String)
+    
+    def __repr__(self):
+        repr_str = f"doc_id={self.doc_id}, headline={self.headline}, body={self.body}"
+
         return repr_str
 
-class TableQueries(Base):
-    __tablename__ = 'doc2queries'
+class WapoDoc2Qeury(Base):
+    __tablename__ = 'wapo_doc2queries'
     docno = Column(String, primary_key=True)
     querygen = Column(ARRAY(Text))
 
     def __repr__(self):
         repr_str = f"docno={self.docno}, query={self.querygen}"
         return repr_str
-    
-class WapoQueries(Base):
-    __tablename__ = 'wapo_doc2queries'
+
+class NytDoc2Qeury(Base):
+    __tablename__ = 'nyt_doc2queries'
     docno = Column(String, primary_key=True)
     querygen = Column(ARRAY(Text))
 
@@ -56,7 +74,8 @@ def init():
         pt.init(boot_packages=["com.github.terrierteam:terrier-prf:-SNAPSHOT"])
 
     global bm25_models
-    global session
+    global session_ds
+    global session_d2q
     global indices
 
     index_path_dic = get_index_paths()
@@ -66,16 +85,42 @@ def init():
         bm25_models[key] = pt.BatchRetrieve(indices[key] , wmodel='BM25', num_results=100)
         print(f"laoded {key} index")
 
-    engine = create_engine("postgresql://root@postgres:5432/wapo")
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    engine_ds = create_engine("postgresql://root@postgres:5432/datasets")
+    Session_ds = sessionmaker(bind=engine_ds)
+    session_ds = Session_ds()
 
+    engine_d2q = create_engine("postgresql://root@postgres:5432/doc2queries")
+    Session_d2q = sessionmaker(bind=engine_d2q)
+    session_d2q = Session_d2q()
+
+@app.route("/results_wapo/<query>", methods=['GET'])
+def response_query_wapo(query: str):
+    #Sgc.collect()
+    results = bm25_models['wapo_v2'].search(query)
+    global last_query
+
+    last_query = query
+    return jsonify(
+        response_query_wapo=results.to_dict()
+    )
+
+#TODO implement nyt index
+@app.route("/results_nyt/<query>", methods=['GET'])
+def response_query_nyt(query: str):
+    #Sgc.collect()
+    results = bm25_models['nyt'].search(query)
+    global last_query
+
+    last_query = query
+    return jsonify(
+        response_query_nyt=results.to_dict()
+    )
 
 @app.route("/doc2query_wapo/<docno_request>", methods=['GET'])
 def response_d2q_wapo(docno_request: str):
     if docno_request:
         docno_request = docno_request.replace("$", "/")
-        result = session.get(WapoQueries, docno_request)
+        result = session_d2q.get(WapoDoc2Qeury, docno_request)
         if result:
             result = result.querygen
         else:
@@ -87,16 +132,22 @@ def response_d2q_wapo(docno_request: str):
         response_d2q_wapo=result
     )
 
-@app.route("/results_wapo/<query>", methods=['GET'])
-def response_query(query: str):
-    #Sgc.collect()
-    results = bm25_models['wapo_v2'].search(query)
-    global last_query
+@app.route("/doc2query_nyt/<docno_request>", methods=['GET'])
+def response_d2q_nyt(docno_request: str):
+    if docno_request:
+        docno_request = docno_request.replace("$", "/")
+        result = session_d2q.get(NytDoc2Qeury, docno_request)
+        if result:
+            result = result.querygen
+        else:
+            return "docno not found"
+    else:
+        return "no argument given"
 
-    last_query = query
     return jsonify(
-        response_query=results.to_dict()
+        response_d2q_nyt=result
     )
+
 
 if __name__ == '__main__':
 
