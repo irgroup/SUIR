67a2b97e351ed0d01e5a125af3c3797be0aa991b
bjoernengelmann <bjoern.engelmann@tu-dortmund.de>
2023-09-18 10:00:36 +0200

    fixed bug: kontext knowledge was not initialized in snippet classifier

--- simulation/simiir/text_classifiers/snippet_classifier.py
+++ simulation/simiir/text_classifiers/snippet_classifier.py
@@ -2,20 +2,80 @@
 import abc
 from simiir.text_classifiers.base_classifier import BaseTextClassifier
 
+from simiir.query_generators.utils import IdfProvider, get_stopwords, filter_keywords
+
+from simiir.search_contexts.search_context import SearchContext
+from collections import Counter
+
+import pickle
+
+
 class SnippetClassifier(BaseTextClassifier):
     """
     TODO: add documentation.
     """
-    def __init__(self, topic, search_context, criterion='title_tf', criterion_threshold=1, rprob=1.0, nprob=1.0, base_seed=0):
+    def __init__(self, topic, search_context, criterion='title_tf', criterion_threshold=1, rprob=1.0, nprob=1.0, base_seed=0, corpus=None):
         """
         TODO: add documentation.
         """
 
         self.criterion = criterion
         self.criterion_threshold = criterion_threshold
+        self._corpus = corpus
+        self._index_path=f'/app/indices/{self._corpus}/data.properties'
+        self._query_path = f"/workspace/data/{self._corpus}/title_queries"
+
+        self._queries = self.get_queries(self._query_path)
+        self._idf_provider = IdfProvider(self._index_path, get_stopwords())
+        topic_path = f"/workspace/data/{self._corpus}/{self._corpus}_topics.pkl"
+        self._topics = pickle.load(open(topic_path, "rb"))
+
+        self.set_context_terms(search_context)
 
         super(SnippetClassifier, self).__init__(topic, search_context)
+    
+    def get_queries(self, query_file):
+        queries = {}
+
+        with open(query_file) as f:
+            queries = {}
+            for line in f.readlines():
+                parts = line.split(',')
+                queries[parts[2]] = parts[3].strip()
+
+        return queries
+
+    def set_context_terms(self, search_context : SearchContext):
+
+        topic_id = search_context.topic.id
+        topic_context = self._topics[self._topics['qid'] == topic_id]
+        start_terms = self._queries[topic_id].split(" ")
         
+        terms_description = set(topic_context['description'].values[0].split(" "))
+        terms_narrative = set(topic_context['narrative'].values[0].split(" "))
+
+        context_terms = terms_narrative.union(terms_description)
+        context_terms = [term for term in context_terms if term not in start_terms]
+
+        if True:
+            #generate list of low signal terms from desription/narrative
+
+            all_terms = []
+
+            for _, row in self._topics.iterrows():
+                all_terms += row['description'].split(" ")
+                all_terms += row['narrative'].split(" ")
+
+            cnt_list = Counter(all_terms)
+            cnt_list = {k:v for k, v in sorted(cnt_list.items(), key=lambda item: -item[1])}
+            
+            context_terms = [term for term in context_terms if cnt_list[term] <= 3]
+
+        context_terms = filter_keywords(self._idf_provider, context_terms)
+        for term in context_terms:
+            search_context.add_context_term(term)
+
+
 
     @abc.abstractmethod
     def is_relevant(self, document):
